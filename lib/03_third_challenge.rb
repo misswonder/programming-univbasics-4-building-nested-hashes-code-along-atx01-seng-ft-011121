780b264359a621548c4ce695f9c96de3db6dc231
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-22 17:54:00 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -27,14 +27,16 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => 
+        :name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         },
       :heroine_friends => []
    }
   }
-
-  
 end
